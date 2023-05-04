1bf8cf317c7b3548951cb5f30e57f41b06bb769e
Danilo Vieira <daanilo.vieira97@gmail.com>
2023-05-03 23:29:48 -0300

    fix: Enviroment

--- src/app/services/api-github.service.ts
+++ src/app/services/api-github.service.ts
@@ -4,6 +4,7 @@ import { UserCardData } from '../models/userCardData';
 import { Observable } from 'rxjs';
 import { GeocodingType } from '../models/geocodingType';
 import { UserRepositoryData } from '../models/userRepositoryData';
+import { Environment } from '../env/enviroment';
 
 @Injectable({
 	providedIn: 'root',
@@ -11,8 +12,7 @@ import { UserRepositoryData } from '../models/userRepositoryData';
 export class ApiGithubService {
 	user: string = '';
 	private baseUserURL: string = 'https://api.github.com/users/';
-	private baseGeoURL: string =
-		'https://maps.googleapis.com/maps/api/geocode/json?key=APIKEY&sensor=false&address=';
+	private baseGeoURL: string = `https://maps.googleapis.com/maps/api/geocode/json?key=${Environment.API_KEY}sensor=false&address=`;
 	userCard: UserCardData | any;
 	userRepo: UserRepositoryData | any;
 	geocoding: GeocodingType | any;
