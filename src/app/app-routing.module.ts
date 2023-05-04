5aabaed8d74e9dec4288a57f096deb97dcea73cb
Danilo Vieira <daanilo.vieira97@gmail.com>
2023-05-03 23:17:59 -0300

    feat: Add routes in Route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UserInfoComponent } from './pages/user-info/user-info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		component: LoginComponent,
+		pathMatch: 'full',
+	},
+	{
+		path: 'register',
+		component: RegisterComponent,
+	},
+	{
+		path: 'home',
+		component: HomeComponent,
+	},
+	{
+		path: 'user/:username',
+		component: UserInfoComponent,
+	},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserInfoComponent } from './pages/user-info/user-info.component';
+import { RepoListComponent } from './components/repo-list/repo-list.component';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, LoginComponent, RegisterComponent, HomeComponent, UserListComponent, UserInfoComponent, RepoListComponent, MapComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		HttpClientModule,
+		FormsModule,
+		ReactiveFormsModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
